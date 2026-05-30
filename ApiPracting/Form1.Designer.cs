6e8f7a4eedcbf97e5252b0d537d17deb160b34f3
Prime <dawiskibamaksim@gmail.com>
2026-05-30 23:50:11 +0300

    Добавьте файлы проекта.

---
+++ ApiPracting/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ApiPracting
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            dataGridViewCoins = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewCoins).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(359, 313);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dataGridViewCoins
+            // 
+            dataGridViewCoins.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCoins.Location = new Point(172, 87);
+            dataGridViewCoins.Name = "dataGridViewCoins";
+            dataGridViewCoins.Size = new Size(461, 175);
+            dataGridViewCoins.TabIndex = 2;
+            dataGridViewCoins.CellContentClick += dataGridViewCoins_CellContentClick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridViewCoins);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridViewCoins).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button button1;
+        private DataGridView dataGridViewCoins;
+    }
+}
---
+++ ApiPracting/Weather.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace ApiPracting
+{
+    public class Weather
+    {
+        [JsonProperty("latitude")]
+        public double Latitude { get; set; }
+
+        [JsonProperty("longitude")]
+        public double Longitude { get; set; }
+
+        [JsonProperty("hourly")]
+        public HourlyData Hourly { get; set; }
+    }
+
+    public class HourlyData
+    {
+        [JsonProperty("time")]
+        public List<string> Time { get; set; }
+
+        [JsonProperty("temperature_2m")]
+        public List<double> Temperature { get; set; }
+
+        [JsonProperty("precipitation_probability")]
+        public List<int> PrecipitationProbability { get; set; }
+    }
+
+    public class HourlyWeatherRow
+    {
+        public string Time { get; set; }
+        public double Temperature { get; set; }
+        public int Precipitation { get; set; }
+    }
+}
